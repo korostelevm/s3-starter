e4613ca7af1af34d69f5bc5011c4ac5291ca536e
Mike <korostelevm@gmail.com>
2022-12-07 10:07:44 -0500

    dev

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const { createPresignedPost } = require("@aws-sdk/s3-presigned-post");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { S3Client, ListObjectsV2Command, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
-const BUCKET_NAME = "cyclic-fair-ruby-clam-cuff-us-east-1";
+const BUCKET_NAME =  process.env.BUCKET_NAME || "cyclic-fair-ruby-clam-cuff-us-east-1";
 
-const REGION = "us-east-1";
+const REGION = process.env.AWS_REGION || "us-east-1";
 
 const s3 = new S3Client({ region: REGION });
 
